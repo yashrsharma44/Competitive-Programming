7cc502493a6b194fc0f2dbabe8a822ded7b69f51
Yash Sharma <yashrsharma44@gmail.com>
2020-07-17 19:55:47 +0530

    17July2020

--- cf/1380/q1.cpp
+++ cf/1380/q1.cpp
@@ -5,7 +5,40 @@ using namespace std;
 
 void solve(){
     
+	int n;
+	cin>>n;
+	vector<int> a;
+	int nn = n;
+	while(nn--){
+		int vl;
+		cin>>vl;
+		a.push_back(vl);
+	}
+
 	
+	for(int i=1;i<n-1;i++){
+		int start = -1, end = -1;
+		for(int j=0;j<=i-1;j++){
+			if(a[j] < a[i]){
+				start = j;
+				break;
+			}
+		}
+		for(int j=i+1;j<n;j++){
+			if(a[j] < a[i]){
+				end = j;
+				break;
+			}
+		}
+
+		if(start != -1 && end != -1){
+			cout<<"YES"<<endl;
+			cout<<start+1<<" "<<i+1<<" "<<end+1<<endl;
+			return;
+		}
+	}
+
+	cout<<"NO"<<endl;
   
 }
 
