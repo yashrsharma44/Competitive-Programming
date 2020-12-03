3043c6ec3bb7c53bfa0cbcd1fe26ca8279a6273a
Yash Sharma <yashrsharma44@gmail.com>
2020-12-03 11:10:09 +0530

    03Dec2020
    Signed-off-by: Yash Sharma <yashrsharma44@gmail.com>

--- ladder/1400-1500/109A.cpp
+++ ladder/1400-1500/109A.cpp
@@ -6,35 +6,6 @@ using namespace std;
 
 void solve(){
 
-	int n,m,k; cin>>n>>m>>k;
-	vector<vector<int>> grid(n,vector<int>(m, -1));
-	for(int i=0;i<n;i++){
-		for(int j=0;j<m;j++){
-			cin>>grid[i][j];
-		}
-	}
-	// vector<int> ans;
-	vector<int> row(n, 0), col(m, 0);
-	for(int i=0;i<n;i++) row[i] = i;
-	for(int j=0;j<m;j++) col[j] = j;
-
-	while(k--){
-		char cc; int x, y;
-		cin>>cc>>x>>y;
-		x--;y--;
-
-		if(cc == 'c'){
-			swap(col[x], col[y]);
-		} else if(cc == 'r'){
-			swap(row[x], row[y]);
-		} else {	
-			x = row[x];
-			y = col[y];
-			cout<<grid[x][y]<<endl;
-		}
-	}
-
-	// for(int el : ans) cout<<el<<endl;
 
 }
 
